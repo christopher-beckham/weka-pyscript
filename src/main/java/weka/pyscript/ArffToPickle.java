18b1278567a8f364f161b57deefd68345280d7f9
Christopher Beckham <chrispy645@gmail.com>
2015-08-19 14:56:10 +1200

    Add filter template and ArffToPickle helper

---
+++ src/main/java/weka/pyscript/ArffToPickle.java
@@ -0,0 +1,61 @@
+package weka.pyscript;
+
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.python.PythonSession;
+
+class ArffToPickleSession {
+	
+	private String m_filename = null;
+	private PythonSession m_session = null;
+	
+	public ArffToPickleSession(String filename) {
+		m_filename = filename;
+	}
+	
+	public void initPythonSession() throws Exception {
+	    if (!PythonSession.pythonAvailable()) {
+	        if (!PythonSession.initSession( "python", true)) {
+	          String envEvalResults = PythonSession.getPythonEnvCheckResults();
+	          throw new Exception("Was unable to start python environment: "
+	            + envEvalResults);
+	        }
+	    } 
+	}
+	
+	public void closePythonSession() {
+		PythonSession.releaseSession(this);
+	}
+	
+	public void test() throws Exception {
+		if(m_session == null) {
+			m_session = PythonSession.acquireSession(this);
+		}
+		DataSource ds = new DataSource(m_filename);
+		Instances instances = ds.getDataSet();
+		m_session.instancesToPython(instances, "df", true);
+		
+	}
+}
+
+/**
+ * Convert an .arff file into a .pkl.gz to be able
+ * to test Python scripts on .arff files without needing
+ * WEKA
+ * @author cjb60
+ *
+ */
+public class ArffToPickle {
+	
+	public static void main(String[] args) throws Exception {
+		
+		ArffToPickleSession session = new 
+				ArffToPickleSession("/Users/cjb60/Desktop/weka/datasets/UCI/iris.arff");
+		
+		//session.initPythonSession();
+		
+		//session.closePythonSession();
+		
+	}
+
+}
