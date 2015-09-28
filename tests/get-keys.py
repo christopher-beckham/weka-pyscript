f80ee387517231d8ba9ee03c1a16f378e61a47ab
Christopher Beckham <chrispy645@gmail.com>
2015-09-28 14:02:45 +1300

    Add scikit-rf.py example to package, add args key test

---
+++ tests/get-keys.py
@@ -0,0 +1,18 @@
+from pyscript.pyscript import ArffToArgs
+
+def get_args(filename, sd, bn, im):
+    x = ArffToArgs()
+    x.set_input(filename)
+    x.set_class_index("last")
+
+    x.set_impute(im)
+    x.set_binarize(bn)
+    x.set_standardize(sd)
+
+    args = x.get_args()
+    x.close()
+    return args
+
+
+args = get_args("../datasets/various.arff", True, True, True)
+print args.keys()
